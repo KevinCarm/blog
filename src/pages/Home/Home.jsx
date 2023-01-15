de4400343d1490f699d6285d4f2f471bb5ca6622
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2023-01-14 20:07:36 -0600

    Add home page and post component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "bulma/css/bulma.css";
+import { Fragment } from "react";
+import HomePost from "../../components/HomePost";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <Fragment>
+            <HomePost />
+        </Fragment>
+    );
+};
+
+export default Home;
