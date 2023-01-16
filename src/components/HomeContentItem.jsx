2790b7e993f2f2b3b84237b3fdf62c1a48d32384
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2023-01-16 14:32:14 -0600

    Add favorite and comments icons in post item

--- src/components/HomeContentItem.jsx
+++ src/components/HomeContentItem.jsx
@@ -2,9 +2,19 @@ import "bulma/css/bulma.css";
 import "./HomeContentItem.css";
 import profile from "../assets/profile.jpg";
 import post from "../assets/post.jpeg";
+import favoriteOff from "../assets/favorite_off.png";
+import favoriteOn from "../assets/favorite_on.png";
+import comments from "../assets/comments.png";
+import { useState } from "react";
 
 const HomeContentItem = () => {
     const contentText = `Kanzi the bonobo lives in America and has learnt how to build a fire, light it using matches and toast marshmallows on it. It shows just how like us some primates really are`;
+
+    const [isFavoriteClick, setIsFavoriteClick] = useState(false);
+    const onFavoriteClickHandler = () => {
+        setIsFavoriteClick(prev => !prev);
+    };
+
     return (
         <div className='container_item'>
             <div>
@@ -21,6 +31,33 @@ const HomeContentItem = () => {
                             src={post}
                             alt=''
                         />
+                    </div>
+                </div>
+                <div className='mt-5 is-flex' style={{ marginLeft: "50px" }}>
+                    <div className='columns'>
+                        <div className='column'>
+                            <img
+                                onClick={onFavoriteClickHandler}
+                                style={{ width: "25px", height: "25px" }}
+                                src={isFavoriteClick ? favoriteOn : favoriteOff}
+                                alt=''
+                            />
+                        </div>
+                        <div className='column'>
+                            <p>654</p>
+                        </div>
+                    </div>
+                    <div className='ml-4 columns'>
+                        <div className='column'>
+                            <img
+                                className='ml-4'
+                                src={comments}
+                                style={{ width: "25px", height: "25px" }}
+                            />
+                        </div>
+                        <div className='column'>
+                            <p>34</p>
+                        </div>
                     </div>
                 </div>
             </div>
